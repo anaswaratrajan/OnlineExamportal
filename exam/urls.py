36b8f3881e3b8878112d96a251713760e7a7e855
anaswaratrajan <anaswaratrajan@gmail.com>
2019-11-23 22:35:48 +0530

    ..

--- exam/urls.py
+++ exam/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.index, name='base'),
-    #path('', views.AddStudent, name='student_form'),
     path('student_form/', views.AddStudent.as_view(), name='student_form')
 ]
